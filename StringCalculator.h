4982cb219fc63639fd28a6a3abc2b48ac74f24c8
Preethiravi04 <preethiravi0499@gmail.com>
2024-07-21 19:29:22 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,37 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-static int parseNumber(const char* str, int* index, char delimiter) {
-    int number = 0;
-    while (str[*index] && str[*index] != delimiter && str[*index] != '\n') {
-        number = number * 10 + (str[*index] - '0');
-        (*index)++;
-    }
-    if (str[*index]) (*index)++;
-    return number;
+static bool isNumber(char ch) {
+    return ch >= '0' && ch <= '9';
 }
 
-static char getCustomDelimiter(const char* str) {
-    return str[2];
+static int addNumbers(int* arr, int count) {
+    int total = 0;
+    for (int i = 0; i < count; i++) {
+        if (arr[i] <= 1000) {
+            total += arr[i];
+        }
+    }
+    return total;
+}
+
+static int extractNumbers(const char* str, int* numArr) {
+    int count = 0;
+    int num = 0;
+    bool negative = false;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '-') {
+            negative = true;
+        } else if (isNumber(str[i])) {
+            num = num * 10 + (str[i] - '0');
+        } else {
+            if (negative) num = -num;
+            numArr[count++] = num;
+            num = 0;
+            negative = false;
+        }
+    }
+    if (isNumber(str[strlen(str) - 1])) {
+        if (negative) num = -num;
+        numArr[count++] = num;
+    }
+    return count;
 }
 
 int add(const char* numbers) {
-    if (!numbers || !numbers[0]) return 0;
-
-    int sum = 0;
-    int index = 0;
-    char delimiter = ',';
-
-    if (numbers[0] == '/' && numbers[1] == '/') {
-        delimiter = getCustomDelimiter(numbers);
-        index = 4;  // Skip the custom delimiter part
+    if (numbers == NULL || strlen(numbers) == 0) {
+        return 0;
     }
 
-    while (numbers[index]) {
-        int num = parseNumber(numbers, &index, delimiter);
-        if (num <= 1000) sum += num;
-    }
+    int numArr[100] = {0};
+    int count = extractNumbers(numbers, numArr);
 
-    return sum;
+    return addNumbers(numArr, count);
 }
