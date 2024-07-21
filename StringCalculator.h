db3a9a4153055801057006219ff4ad0429e846c8
Preethiravi04 <preethiravi0499@gmail.com>
2024-07-21 20:46:37 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,99 +1,145 @@
-#define MAX_NUMBERS 100
-static bool isNumber(char ch) {
-    return ch >= '0' && ch <= '9';
-}
-
-static int addNumbers(int* arr, int count) {
+int add_arr(int *arr,int cnt)
+{
+    int i = 0;
     int total = 0;
-    for (int i = 0; i < count; i++) {
-        if (arr[i] <= 1000) {
-            total += arr[i];
+    while(i < cnt)
+    {
+        if(arr[i] < 1000)
+        {
+          total += arr[i];
         }
+        i++;
     }
+ 
     return total;
 }
-
-#include <cstring>
-
-// Helper function to process the number
-static void processNumber(int &num, bool &negative, int* &numArr, int &count) {
-    if (negative) num = -num;
-    numArr[count++] = num;
-    num = 0;
-    negative = false;
-}
-
-static void addNumber(int &num, bool negative, int* numArr, int &count) {
-    if (negative) num = -num;
-    numArr[count++] = num;
-    num = 0;
-}
-
-// Helper function to process each character
-void handleNegative(bool &negative) {
-    negative = true;
-}
-
-// Function to process digits
-void handleDigit(char ch, int &num) {
-    num = num * 10 + (ch - '0');
-}
-
-// Function to handle the end of a number
-void handleEndOfNumber(int &num, bool &negative, int* numArr, int &count) {
-    addNumber(num, negative, numArr, count);
-    num = 0; // Reset num for the next number
-    negative = false; // Reset negative flag for the next number
-}
-
-bool IsDelimiter(char ch, char delimiter)
+ 
+ 
+//check if its a number
+int check_num(const char ch)
 {
-    return (ch == delimiter || ch == '\n');
+    if(ch >= '0' && ch <= '9')
+    {
+        return 1;
+    }
+    return 0;
 }
-void processCharacter(char ch, int &num, bool &negative, int* numArr, int &count, char delimiter) {
-    if (ch == '-') {
-        handleNegative(negative);
-    } else if (isdigit(ch)) {
-        handleDigit(ch, num);
-    } else if (IsDelimiter(ch, delimiter)) {
-        handleEndOfNumber(num, negative, numArr, count);
+ 
+void strtonum(const char* ch, int *num_arr,int *cnt)
+{
+    static int num = 0;
+ 
+    if(check_num(ch[*cnt]))
+    {
+       num = num * 10 + ch[*cnt] - '0';
+       if(check_num(ch[(*cnt)+1]))
+       {
+           (*cnt)++;
+       }
+       else
+       {
+          num_arr[*cnt] = num;
+          num = 0;
+          (*cnt)++;
+       }
+    }
+    else
+    {
+        (*cnt)++;
+        num = 0;
     }
 }
-
-static int extractNumbers(const char* str, int* numArr, char delimiter) {
-    int count = 0;
-    int num = 0;
-    bool negative = false;
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        processCharacter(str[i], num, negative, numArr, count, delimiter);
+ 
+int sum(const char *ch)
+{
+    int cnt =0;
+    int total = 0;
+    int num_arr[100] = {0};
+    while(ch[cnt] != '\0')
+    {
+       strtonum(ch,num_arr,&cnt);
     }
-
-    // Add the last number if necessary
-    if (isdigit(str[strlen(str) - 1])) {
-        addNumber(num, negative, numArr, count);
+ 
+    total = add_arr(num_arr,cnt);
+ 
+    return total;
+}
+ 
+int neg_loop(const char *ch,int i)
+{
+      if(ch[i] == '-')
+      {
+          if(check_num(ch[i+1]))
+          {
+              return 0;
+          }
+      }
+    return 1;
+}
+ 
+int check_neg(const char *ch)
+{
+    int res = 0,i=0;
+    while(ch[i] != '\0')
+    {
+      res = neg_loop(ch,i);
+      if(res == 0)
+      {
+          break;
+      }
+      i++;
     }
-
-    return count;
+    return res;
 }
-static char getCustomDelimiter(const char* numbers) {
-    return numbers[2];
+ 
+int check_empty(const char* ch)
+{
+    int strlength = strlen(ch);
+ 
+    if(strlength == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+ 
+int check_neg_empty(const char *ch)
+{
+       if(check_neg(ch))
+       {
+           return 1;
+       }
+       else if(check_empty(ch))
+       {
+           return 1;
+       }
+ 
+       return 0;
+}
+ 
+int check_string(const char *ch)
+{
+    if(ch != NULL)
+    {
+        return check_neg_empty(ch);
+    }
+ 
+    return 0;
 }
 
-int add(const char* numbers) {
-    if (numbers == NULL || strlen(numbers) == 0) {
+ 
+int add(const char *ch)
+{
+    if(check_string(ch))
+    {
+        int total = sum(ch);
+ 
+        if(total > 0)
+        {
+            return total;
+        }
         return 0;
     }
-
-    int numArr[MAX_NUMBERS] = {0};
-    int count = 0;
-    char delimiter = ',';
-
-    if (numbers[0] == '/' && numbers[1] == '/') {
-        delimiter = getCustomDelimiter(numbers);
-        numbers += 4;
-    }
-
-    count = extractNumbers(numbers, numArr, delimiter);
-    return addNumbers(numArr, count);
+ 
+    return -1;
 }
